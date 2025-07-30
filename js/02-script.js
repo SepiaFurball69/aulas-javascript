e8440d9a6313da6a0bb9d32034e1b851df568293
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-07-30 16:53:40 -0300

    habilitando modo estrito e aspas pra texto

--- js/02-script.js
+++ js/02-script.js
@@ -1,1 +1,29 @@
-console.log("Hello world");
+//habilitando o modo estrito
+'use strict';
+
+let teste = "senac";
+console.log(teste);
+
+let userInterface = "teste"
+
+
+/* exercícios */
+// 1)crie uma variavel chamada "uc" e coloque nela o numero da UC que nós estamos
+// 2)crie uma variavel chamada "competencia" e coloque nela a frase que represente o que estamos estudando nesta UC
+// 3)monte e mostre uma frase no console exibindo a UC e a competencia. Exemplo: Estamos na UC2 estudando Front-End com javaScript.
+
+let uc = 2
+let competencia = "estudando Front-End com JavaScript"
+
+console.log(`Estamos no Senac Penha na UC${uc} e estamos ${competencia} com o professor Thiago`);
+
+// colocar aspas como texto
+//ERRADO!
+//console.log("gosto "muito" de paçoca);
+// CORRETO! Aspas simples como comando, e duplas como texto
+//console.log('gosto "muito" de paçoca');
+//CORRETO! Aspas duplas como comando, simples como texto
+//console.log("gosto 'muito' de paçoca");
+//CORRETO! Escapando as aspas como texto
+//console.log("Gosto \"muito\" de paçoca")
+
