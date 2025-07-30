27fa939388efa3eed03be018c2476de11b940466
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-07-30 17:26:39 -0300

    brincando com vetores atravez das arrays

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// Declarando um array
+// Declarando um array (vetor)
+let osfoda = ["arana", "blakes", "gp da zl", "silvestre", "leo da 17", "mandrake", "petrus", "novin"];
+
+// console.log(osfoda[0]);
+//console.log(osfoda);
+
+// Acessando um determinado elemento através do indice
+console.log(osfoda[0]);
+
+
+// O silvestre está em turne tocando taca fogo em kiksilver
+console.log(`o ${osfoda[3]} está em turne tocando taca fogo em kiksilver`);
