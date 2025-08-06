f0260898052564e1aa296f59281b6bb66f3df05f
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-06 15:30:00 -0300

    criando arrays como matrizes de 2 dimensoes
    como criar arrays como matrizes de duas dimensoes

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -17,3 +17,22 @@ const coisas = [10, "senac", "<h2>oie</h2>", seila, 15.88];
 console.log(coisas[1]);
 console.log(coisas[4]);
 
+// exercicios
+// 1) Crie um array contendo um nome de 7 coisas que vc gosta. exemplo: artistas, musicas, livros, comida, etc...
+let gostos = ['futebol', 'são paulo', 'brasil', 'libertadores', 'chelsea', 'champions league', 'real madrid']
+
+// 2) mostre no consile uma frase personalizada indicando o segundo, o quinto e o setimo elemento do array. use concatenação e/ou template string
+
+console.log("eu torço para o "+gostos[1]+" meu time da inglaterra é o "+gostos[4]+" e o meu time da espanha é o "+gostos[6]);
+
+// array como matriz de 2 dimensoes
+const tecnologias = [
+    ["HTML", "CSS", "JAVASCRIPT"],
+    ["FIGMA", "PHOTOSHOP"],
+    ["PHP", "NODE.js", "SQL", "Express"]
+]
+console.log(tecnologias[0] [2]); //JavaScript
+console.log(tecnologias[1] [0]); //Figma
+console.log(tecnologias[2] [3]); //Express
+
+
