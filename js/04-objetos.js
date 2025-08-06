9eb808561d05f23cb07b4e4ac3ab3a0adaaa59cb
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-06 17:26:58 -0300

    feito exercicios sobre objetos e arrays

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -3,7 +3,7 @@
 // Exemplo 1: objeto com dados de uma pessoa
 const pessoa = {
     // propriedades: valor
-    nome: "chaves",
+    n: "chaves",
     idade: 8,
     cidade: "são paulo",
     estado: "sp"
@@ -37,4 +37,50 @@ console.log(livro.titulo);
 
 // Para acessaralgum dos volumes, primeiro passamos pela prorpiedade (volumes) usando 'ponto' e, chegando nela, usamos os 'colchetes' com indice pois se trata de um array.
 console.log(`Meu livro preferido é ${livro.volumes[3]}`);
+
+//Exemplo 3: array com objetos
+console.log("\nExemplo3\n");
+const livros = [
+    {
+        titulo: "o assassinato no expresso do oriente",
+        autor: "agatha christie"
+    },
+    {
+        titulo: "crepusculo",
+        autor: "Stephanie myers"
+    },
+    {
+        titulo: "bla",
+        autor: "bin"
+    }
+]
+console.log(livros);
+console.log(livros[2].titulo);
+
+/* exercicios
+1. Crie um objeto chamado 'aluno' contendo os seguintes dados:
+-nome completo
+-data de nascimento
+-(use array) lista de telefones (fixo e celular)
+-(desafio: use um objeto nesta propriedade) endereço contendo separadamente: rua, numero, bairro.
+2. mostre no console o nome do aluno, o telefone celular e o bairro em que mora.
+*/
+const aluno = {
+
+    nome: "Henrique de Sousa Freitas",
+    data: "03/09/2003",
+    contato: [
+        "11972727890",
+        "40028922"
+    ],
+    endereco: {
+        rua: "paulo vicente",
+        numero: 400,
+        bairro: "Vila Carolina"
+    }
+};
+console.log(`O Aluno se chama ${aluno.nome} o numero de celular dele é ${aluno.contato[0]} e ele mora no bairro ${aluno.endereco.bairro}`);
+
+
+
 
