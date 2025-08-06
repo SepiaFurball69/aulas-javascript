a81309f0588326b23845680f978f5ef8d939720d
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-06 14:33:31 -0300

    relembrando arrays
    relembrando como usar e criar arrays como vetor, e mostrando que podemos incluir qualquer coisa em um array

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -2,13 +2,18 @@
 
 // Declarando um array (vetor)
 let osfoda = ["arana", "blakes", "gp da zl", "silvestre", "leo da 17", "mandrake", "petrus", "novin"];
-
+console.log(osfoda);
 // console.log(osfoda[0]);
 //console.log(osfoda);
-
 // Acessando um determinado elemento através do indice
 console.log(osfoda[0]);
-
-
 // O silvestre está em turne tocando taca fogo em kiksilver
 console.log(`o ${osfoda[3]} está em turne tocando taca fogo em kiksilver`);
+
+// relembrando como criar/usar arrays (vetor)
+// no js, vc pode colocar qualquer coisa em um array
+let seila = 6;
+const coisas = [10, "senac", "<h2>oie</h2>", seila, 15.88];
+console.log(coisas[1]);
+console.log(coisas[4]);
+
