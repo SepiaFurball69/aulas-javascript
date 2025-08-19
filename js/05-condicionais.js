d483f26215c01f9cbcbd2b4ae6a94cc7aa7611e1
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-19 14:37:05 -0300

    adicionando condicional

---
+++ js/05-condicionais.js
@@ -0,0 +1,9 @@
+'use strict';
+console.log("teste");
+
+let numero = 5;
+
+console.log("Exemplo 1");
+if (numero >= 10) {
+    console.log("Se voce está vendo essa mensagem é porque a condição é verdadeira");
+}
