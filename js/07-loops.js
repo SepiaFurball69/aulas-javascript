b77b2cf1eb1e19027489a58f50ccf6276bef5117
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-20 14:13:29 -0300

    fazendo variavel contadora

--- js/07-loops.js
+++ js/07-loops.js
@@ -2,6 +2,7 @@
 
 /* Loops ou estruturas de repetição
 são comandos que, baseados em alguma condição logica, são programados para executar ações repetidas vezes.
+comandos masi tradicionais: while (enquanto) e for (para)
 
-
+normalment, os loops são controlados atravez de uma variavel contadora
 */
