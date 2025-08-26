2f716f2179807078c5c725c6813566b8eb7351d7
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-26 16:23:56 -0300

    feito formatação de moeda e funcoes extras

--- js/08-funcoes.js
+++ js/08-funcoes.js
@@ -9,7 +9,7 @@
 
 console.log("exemplo 1: função anonima");
 
-const exemplo1 = function(){
+const exemplo1 = function () {
     //corpo da função: ação que a função vai fazer
     console.log("olá função anonima");
 };
@@ -35,9 +35,70 @@ exemplo3();
 quando uma função precisa de valores/dados para algum tipo de processamento, ela recebe valores/dados atraves de parametros/argumentos entre os parenteses
 
 geralmente, ao terminar o processamento de dados, a função "retorna" para fora do resultado.*/
+console.log("\nExemplo 4: função com parametro");
 
-function saudacao( nome ) {
-    console.log("olá, "+nome);
+function saudacao(nome = "Visitante") {
+    console.log("olá, " + nome);
 }
 
 saudacao("Henrique");
+saudacao(); //nesse caso, é usado "visitante" como valor
+
+console.log("\nExemplo 5: função com parametros e retorno");
+
+function multiplicar(valor1, valor2) {
+    return valor1 * valor2
+};
+
+let resultado1 = multiplicar(10, 5);
+let resultado2 = multiplicar(200, 10);
+
+console.log("resultado1: " + resultado1);
+console.log("resultado2: " + resultado2);
+
+console.log("\nExemplo 6: simplificando com Arrow function");
+
+//versão 1: Sintaxe declarada/nomeada
+/* function somar(valor1, valor2){
+    return valor1 + valor2;
+}; */
+//versão 2: sintaxe Arrow function
+const somar = (valor1, valor2) => valor1 + valor2;
+console.log(somar(150, 500));
+
+let preco = 5000;
+let desconto = preco * 0.10; //10%
+let precofinal = preco - desconto;
+
+function formatarMoeda(valor) {
+    return new Intl.NumberFormat("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    }).format(valor);
+}
+
+console.log("preco original: " + formatarMoeda(preco));
+console.log("Desconto: " + formatarMoeda(desconto));
+console.log("preço final: " + formatarMoeda(precofinal));
+
+// Exemplo: usando recursos de classe Intl (Internacionalização)
+const exemplo = new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL"
+}).format(preco);
+
+console.log(exemplo);
+
+const exemploB = new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL"
+}).format(desconto);
+
+console.log(exemploB);
+
+const exemploC = new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL"
+}).format(precofinal);
+
+console.log(exemploC);
