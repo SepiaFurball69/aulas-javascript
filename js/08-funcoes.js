59853d703497d00b70765ba4f2d93c57cc43b5af
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-26 14:31:49 -0300

    feito funções anonima e declaradas

--- js/08-funcoes.js
+++ js/08-funcoes.js
@@ -1,2 +1,24 @@
 'use strict'
-console.log("teste!");
+// sobre funções: funções são blocos de codigo responsáveis por executar determinadas tarefas. para usar estes blocos/funções, é necessário "chamar/invocar" a função desejada atraves do seu nome.
+
+/*vantagens ao usar funções: 
+-reutilização de código
+-organização de codigo-fonte, permitindo a separação de diferentes lógicas e responsabilidades
+-performance melhor
+-maior facilidade para manutenções */
+
+console.log("exemplo 1: função anonima");
+
+const exemplo1 = function(){
+    //corpo da função: ação que a função vai fazer
+    console.log("olá função anonima");
+};
+//chamando/invocando a função
+exemplo1();
+
+console.log("\nExemplo 2: função Nomeada/Declarada");
+function() {
+    console.log("esta é a função nomeada!");
+}
+exemplo2();
+
