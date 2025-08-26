4c4f0ec728ad256972842f23a54c0a9c1cb7a41c
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-26 17:25:53 -0300

    testando

--- js/09-dom-eventos.js
+++ js/09-dom-eventos.js
@@ -1,2 +1,13 @@
 'use strict';
 
+/* exemplo 01 */
+// Procurando (querySelector) por um elemento chamado exemplo1
+const exemplo01 = document.querySelector("#exemplo1");
+const mensagem01 = document.querySelector("#mensagem01");
+const pagina = document.querySelector("body");
+
+console.log(exemplo01);
+console.log(mensagem01);
+console.log(pagina);
+
+
