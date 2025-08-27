4e2de2416aa9c0212690e1bcdd021c8482589575
HENRIQUE DE SOUSA FREITAS <henrique.sfreitas@senacsp.edu.br>
2025-08-27 14:04:00 -0300

    utilizando querySelector e o getelement

--- js/09-dom-eventos.js
+++ js/09-dom-eventos.js
@@ -6,8 +6,7 @@ const exemplo01 = document.querySelector("#exemplo1");
 const mensagem01 = document.querySelector("#mensagem01");
 const pagina = document.querySelector("body");
 
-console.log(exemplo01);
-console.log(mensagem01);
-console.log(pagina);
 
 
+const paragrafo = document.querySelectorAll("p");
+console.log(paragrafo);
